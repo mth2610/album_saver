4d7b588d5ac775be1c9b29b4d75a4449555ad9d9
MacOS <macos@MacOSs-MacBook-Pro.local>
2019-04-13 15:07:30 +0700

    fix dependencies

--- test/album_saver_test.dart
+++ test/album_saver_test.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:album_saver/album_saver.dart';
 
 void main() {
   const MethodChannel channel = MethodChannel('album_saver');
@@ -15,7 +14,4 @@ void main() {
     channel.setMockMethodCallHandler(null);
   });
 
-  test('getPlatformVersion', () async {
-    expect(await AlbumSaver.platformVersion, '42');
-  });
 }
